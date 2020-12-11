35e8ad3e20ae231375691774f32305ada04476bb
Klecia Vladmaker <klecia.vladmaker@hotmail.com>
2020-12-11 10:32:07 -0300

    Projeto ByteBank

---
+++ src/ContaCorrente.java
@@ -0,0 +1,25 @@
+
+public class ContaCorrente extends Conta implements Tributavel {
+
+	public ContaCorrente(int agencia, int numero) {
+		super(agencia, numero);
+	}
+	
+	@Override
+	public boolean saca(double valor) {
+		return super.saca(valor + 0.2);
+	}
+
+	@Override
+	public void deposita(double valor) {
+		super.saldo += valor;
+		
+	}
+
+	@Override
+	public double getValorImposto() {
+		// TODO Auto-generated method stub
+		return super.saldo * 0.01;
+	}
+	
+}
